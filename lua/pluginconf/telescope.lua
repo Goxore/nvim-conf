54da44f4e6b0db4ab534d154efe7282847f78aff
Yurii <ggooxxoorree@gmail.com>
2022-11-23 15:30:40 +0100

    telescope multiselect

--- lua/pluginconf/telescope.lua
+++ lua/pluginconf/telescope.lua
@@ -4,45 +4,180 @@ if not status_ok then
 end
 
 local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local transform_mod = require("telescope.actions.mt").transform_mod
+
+debugcommand = "nothing"
+
+local function multiopen(prompt_bufnr, method)
+    local edit_file_cmd_map = {
+        vertical = "vsplit",
+        horizontal = "split",
+        tab = "tabedit",
+        default = "edit",
+    }
+    local edit_buf_cmd_map = {
+        vertical = "vert sbuffer",
+        horizontal = "sbuffer",
+        tab = "tab sbuffer",
+        default = "buffer",
+    }
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local multi_selection = picker:get_multi_selection()
+
+    if #multi_selection > 1 then
+        require("telescope.pickers").on_close_prompt(prompt_bufnr)
+        pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+        for i, entry in ipairs(multi_selection) do
+            local filename, row, col
+
+            if entry.path or entry.filename then
+                filename = entry.path or entry.filename
+
+                row = entry.row or entry.lnum
+                col = vim.F.if_nil(entry.col, 1)
+            elseif not entry.bufnr then
+                local value = entry.value
+                if not value then
+                    return
+                end
+
+                if type(value) == "table" then
+                    value = entry.display
+                end
+
+                local sections = vim.split(value, ":")
+
+                filename = sections[1]
+                row = tonumber(sections[2])
+                col = tonumber(sections[3])
+            end
+
+            local entry_bufnr = entry.bufnr
+
+            if entry_bufnr then
+                if not vim.api.nvim_buf_get_option(entry_bufnr, "buflisted") then
+                    vim.api.nvim_buf_set_option(entry_bufnr, "buflisted", true)
+                end
+                local command = i == 1 and "buffer" or edit_buf_cmd_map[method]
+                pcall(vim.cmd, string.format("%s %s", command, vim.api.nvim_buf_get_name(entry_bufnr)))
+            else
+                local command = i == 1 and "edit" or edit_file_cmd_map[method]
+                if vim.api.nvim_buf_get_name(0) ~= filename or command ~= "edit" then
+                    filename = require("plenary.path"):new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+                    pcall(vim.cmd, string.format("%s %s", command, filename))
+                    debugcommand = string.format("%s %s", command, filename:gsub("\\\\", "\\"))
+                end
+            end
+
+            if row and col then
+                pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
+            end
+        end
+    else
+        actions["select_" .. method](prompt_bufnr)
+    end
+end
+
+local custom_actions = transform_mod({
+    multi_selection_open_vertical = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "vertical")
+    end,
+    multi_selection_open_horizontal = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "horizontal")
+    end,
+    multi_selection_open_tab = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "tab")
+    end,
+    multi_selection_open = function(prompt_bufnr)
+        multiopen(prompt_bufnr, "default")
+    end,
+})
+
+local function stopinsert(callback)
+    return function(prompt_bufnr)
+        vim.cmd.stopinsert()
+        vim.schedule(function()
+            callback(prompt_bufnr)
+        end)
+    end
+end
+
+local multi_open_mappings = {
+    i = {
+        ["<C-v>"] = stopinsert(custom_actions.multi_selection_open_vertical),
+        ["<C-s>"] = stopinsert(custom_actions.multi_selection_open_horizontal),
+        ["<C-t>"] = stopinsert(custom_actions.multi_selection_open_tab),
+        ["<CR>"]  = stopinsert(custom_actions.multi_selection_open)
+    },
+    n = {
+        ["<C-v>"] = custom_actions.multi_selection_open_vertical,
+        ["<C-s>"] = custom_actions.multi_selection_open_horizontal,
+        ["<C-t>"] = custom_actions.multi_selection_open_tab,
+        ["<CR>"] = custom_actions.multi_selection_open,
+    },
+}
+
+--
+-- local telescope_custom_actions = {}
+--
+-- function telescope_custom_actions._multiopen(prompt_bufnr, open_cmd)
+--     local picker = action_state.get_current_picker(prompt_bufnr)
+--     local selected_entry = action_state.get_selected_entry()
+--     local num_selections = #picker:get_multi_selection()
+--     if not num_selections or num_selections <= 1 then
+--         actions.add_selection(prompt_bufnr)
+--     end
+--     actions.send_selected_to_qflist(prompt_bufnr)
+--     vim.cmd("silent! cfdo " .. open_cmd)
+-- end
+-- function telescope_custom_actions.multi_selection_open_vsplit(prompt_bufnr)
+--     telescope_custom_actions._multiopen(prompt_bufnr, "vsplit")
+-- end
+-- function telescope_custom_actions.multi_selection_open_split(prompt_bufnr)
+--     telescope_custom_actions._multiopen(prompt_bufnr, "split")
+-- end
+-- function telescope_custom_actions.multi_selection_open_tab(prompt_bufnr)
+--     telescope_custom_actions._multiopen(prompt_bufnr, "tabe")
+-- end
+-- function telescope_custom_actions.multi_selection_open(prompt_bufnr)
+--     telescope_custom_actions._multiopen(prompt_bufnr, "edit")
+-- end
 
 telescope.setup{
   defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
     mappings = {
       i = {
         ["<C-h>"] = "which_key",
-
         ["<C-j>"] = {
           actions.move_selection_next, type = "action",
           opts = { nowait = true, silent = true }
         },
-
         ["<C-k>"] = {
           actions.move_selection_previous, type = "action",
           opts = { nowait = true, silent = true }
         },
+        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist
       }
     }
   },
   pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
+    find_files = {
+          mappings = multi_open_mappings
+      },
+    oldfiles = {
+          mappings = multi_open_mappings
+      }
   },
   extensions = {
     fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
     }
   }
 }
 
--- telescope.load_extension('fzf')
+telescope.load_extension('fzf')
